a8c1c83c35b9e84a8f1910fb0da4fbd32c932321
Eduardo-Goncalvess <edusilvarocha@hotmail.com>
2024-12-15 17:20:14 -0300

    Add files via upload

---
+++ Inicio.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth < 768) {
+        window.location.href = "https://eduardo-goncalvess.github.io/EcoIrriga/TS.html";
+    }
+});
